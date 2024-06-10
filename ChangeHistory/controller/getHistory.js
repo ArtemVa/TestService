795e43dbc9d4dcabe4c4891e6553da4158acb88f
ArtemVa <49078895+ArtemVa@users.noreply.github.com>
2024-06-10 10:43:58 +0300

    Update getHistory.js
    incorrect if-else

--- ChangeHistory/controller/getHistory.js
+++ ChangeHistory/controller/getHistory.js
@@ -7,7 +7,7 @@ async function getHistory(req, res){
         const {page, id} = req.query;
         const offset = (page-1)*limit;
         const result = await pool.query('select * from history where id = $1 limit $2 offset $3', [id, limit, offset]);
-        if(!!result){
+        if(Object.keys(result).length == 0){
             res.send('Данных больше нет')
         }else{
             res.send(result.rows)
